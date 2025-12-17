2aa1ddb847ed82fade86d455ef4da8cb3082a38d
Mario T. Lanza <mario@doesideas.com>
2025-12-17 09:10:04 -0500

    save

--- main.js
+++ main.js
@@ -3,6 +3,15 @@ import $ from "./libs/atomic_/shell.js";
 import {reg} from "./libs/cmd.js";
 import * as o from "./ocd.js";
 
-const $state = $.atom(null);
+const $state = $.atom(o.init(1));
 
 reg({$state});
+
+$.swap($state, o.rollEnergyDice);
+$.swap($state, o.assignEnergyDie(2));
+$.swap($state, o.assignEnergyDie(1));
+$.swap($state, o.assignEnergyDie(3));
+$.swap($state, function(state){
+
+})
+
---
+++ ocd.js
@@ -0,0 +1,21 @@
+export function init(level){
+  return {
+    start: [0, 4],
+    dungeon: [[null, null, null, "W", null]]
+  };
+}
+
+
+function rollEnergyDice(state){
+  state.dungeon = [];
+  const {start, dungeon} = state;
+  const [r1, r2, r3, r4, r5] = dungeon;
+  const newStart = [5, 6];
+  return {...state, start: newStart, energyDicePool: [1, 5, 6], assignedEnergyDice: []}
+}
+
+function assignEnergyDie(slot){
+  return function(state){
+    return {...state, start: newStart, energyDicePool: [5, 6], assignedEnergyDice: [null, 1, null]}
+  }
+}
