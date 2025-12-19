e5c63caae10fdf5036fb0306141b8bc7899612ce
Mario T. Lanza <mario@doesideas.com>
2025-12-19 08:26:38 -0500

    save

--- main.js
+++ main.js
@@ -7,11 +7,20 @@ const $state = $.atom(o.init());
 
 reg({$state});
 
+function rolling(...dice){
+  let rolls = _.cycle(dice);
+  return function(){
+    const roll = _.first(rolls);
+    rolls = _.rest(rolls);
+    return roll;
+  }
+}
+
 function die(sides = 6){
   return _.randInt(sides) + 1;
 }
 
-$.swap($state, o.energize(die));
+$.swap($state, o.energize(rolling(4, 5, 6)));
 $.swap($state, o.assignEnergy("speed"));
 $.swap($state, o.assignEnergy("attack"));
 $.swap($state, o.assignEnergy("defense"));
