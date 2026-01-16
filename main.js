804be72421d5ae99758c9e62e625589c8828a6d6
Mario T. Lanza <me@mlanza.com>
2026-01-16 08:38:48 -0500

    save

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import $ from "./libs/atomic_/shell.js";
 import * as eff from "./libs/effects.js";
 import { reg } from "./libs/cmd.js";
 import * as o from "./ocd.js";
+const v = null;
+import {H, X} from "./ocd.js";
 
 const $state = $.atom(o.init());
 
@@ -13,3 +15,11 @@ $.swap($state, o.assignEnergy("speed"));
 $.swap($state, o.assignEnergy("attack"));
 $.swap($state, o.assignEnergy("defense"));
 //$.swap($state, o.move({type: "move", details: {occupant: o.H, offset: [-1, 0], speed: 2}}));
+
+_.chain(los([2, 2], [4, 1], [
+  [0, X, v, v, 4],
+  [v, v, v, X, v],
+  [1, X, H, X, v],
+  [v, X, X, v, v],
+  [v, 2, v, v, 3]
+]), _.not, assert);
--- tests/los.js
+++ tests/los.js
@@ -5,7 +5,7 @@ import {
 import _ from "../libs/atomic_/core.js";
 import $ from "../libs/atomic_/shell.js";
 import { los, X, H } from "../ocd.js";
-const o = null; //open
+const v = null; //vacant
 
 // BGG Thread: "Line of Sight clarification with examples" - markers for test cases
 const B = 2; // Monster marker
@@ -18,105 +18,105 @@ const E = 4; // Monster marker
 Deno.test("LOS - BGG A: clear diagonal LOS", () => {
   // A to B: Clear diagonal path
   _.chain(los([2, 2], [0, 0], [
-    [0, X, o, o, 4],
-    [o, o, o, X, o],
-    [1, X, H, X, o],
-    [o, X, X, o, o],
-    [o, 2, o, o, 3]
+    [0, X, v, v, 4],
+    [v, v, v, X, v],
+    [1, X, H, X, v],
+    [v, X, X, v, v],
+    [v, 2, v, v, 3]
   ]), _.not, _.not, assert);
 });
 
 Deno.test("LOS - BGG B: no LoS due to wall", () => {
   // A to B: No LOS due to blocking wall
   _.chain(los([2, 2], [4, 1], [
-    [0, X, o, o, 4],
-    [o, o, o, X, o],
-    [1, X, H, X, o],
-    [o, X, X, o, o],
-    [o, 2, o, o, 3]
+    [0, X, v, v, 4],
+    [v, v, v, X, v],
+    [1, X, H, X, v],
+    [v, X, X, v, v],
+    [v, 2, v, v, 3]
   ]), _.not, assert);
 });
 
 Deno.test("LOS - BGG C: LoS due to monster", () => {
   // A to C: Monster blocks LOS to B
   _.chain(los([2, 2], [2, 0], [
-    [0, X, o, o, 4],
-    [o, o, o, X, o],
-    [1, X, H, X, o],
-    [o, X, X, o, o],
-    [o, 2, o, o, 3]
+    [0, X, v, v, 4],
+    [v, v, v, X, v],
+    [1, X, H, X, v],
+    [v, X, X, v, v],
+    [v, 2, v, v, 3]
   ]), _.not, assert);
 });
 
 Deno.test("LOS - BGG D: corner squeeze", () => {
   // A to D: Complex corner squeeze around obstacles
   _.chain(los([2, 2], [4, 4], [
-    [0, X, o, o, 4],
-    [o, o, o, X, o],
-    [1, X, H, X, o],
-    [o, X, X, o, o],
-    [o, 2, o, o, 3]
+    [0, X, v, v, 4],
+    [v, v, v, X, v],
+    [1, X, H, X, v],
+    [v, X, X, v, v],
+    [v, 2, v, v, 3]
   ]), _.not, _.not, assert);
 });
 
 Deno.test("LOS - BGG E: edge grazing", () => {
   // A to E: Edge grazing case (no LOS per clarification)
   _.chain(los([2, 2], [0, 4], [
-    [0, X, o, o, 4],
-    [o, o, o, X, o],
-    [1, X, H, X, o],
-    [o, X, X, o, o],
-    [o, 2, o, o, 3]
+    [0, X, v, v, 4],
+    [v, v, v, X, v],
+    [1, X, H, X, v],
+    [v, X, X, v, v],
+    [v, 2, v, v, 3]
   ]), _.not, assert);
 });
 
 Deno.test("LOS - same tile should have LOS", () => {
   _.chain(los([0, 4], [0, 4], [
-    [o, o, o, o, o],
-    [o, o, o, o, o],
-    [o, o, o, o, o],
-    [o, o, o, o, o],
-    [H, o, o, o, o]
+    [v, v, v, v, v],
+    [v, v, v, v, v],
+    [v, v, v, v, v],
+    [v, v, v, v, v],
+    [H, v, v, v, v]
   ]), _.not, _.not, assert);
 });
 
 Deno.test("LOS - clear horizontal line of sight", () => {
   _.chain(los([0, 0], [4, 0], [
-    [H, o, o, o, 1],
-    [o, o, o, o, o],
-    [o, o, o, o, o],
-    [o, o, o, o, o],
-    [o, o, o, o, o]
+    [H, v, v, v, 1],
+    [v, v, v, v, v],
+    [v, v, v, v, v],
+    [v, v, v, v, v],
+    [v, v, v, v, v]
   ]), _.not, _.not, assert);
 });
 
 Deno.test("LOS - clear vertical line of sight", () => {
   _.chain(los([0, 0], [0, 4], [
-    [H, o, o, o, o],
-    [o, o, o, o, o],
-    [o, o, o, o, o],
-    [o, o, o, o, o],
-    [1, o, o, o, o]
+    [H, v, v, v, v],
+    [v, v, v, v, v],
+    [v, v, v, v, v],
+    [v, v, v, v, v],
+    [1, v, v, v, v]
   ]), _.not, _.not, assert);
 });
 
 Deno.test("LOS - clear diagonal line of sight", () => {
   _.chain(los([0, 0], [4, 4], [
-    [H, o, o, o, o],
-    [o, o, o, o, o],
-    [o, o, o, o, o],
-    [o, o, o, o, o],
-    [o, o, o, o, 1]
+    [H, v, v, v, v],
+    [v, v, v, v, v],
+    [v, v, v, v, v],
+    [v, v, v, v, v],
+    [v, v, v, v, 1]
   ]), _.not, _.not, assert);
 });
 
 Deno.test("LOS - wall blocking horizontal LOS", () => {
   _.chain(los([1, 1], [4, 0], [
-    [o, o, X, o, 1],
-    [H, o, X, o, o],
-    [o, o, X, o, o],
-    [o, o, X, o, o],
-    [o, o, o, o, o]
+    [v, v, X, v, 1],
+    [H, v, X, v, v],
+    [v, v, X, v, v],
+    [v, v, X, v, v],
+    [v, v, v, v, v]
   ]), _.not, assert);
 });
 
@@ -125,146 +125,146 @@ Deno.test("LOS - wall blocking vertical LOS", () => {
   // Test expects true (clear LOS) but wall at [0][2] blocks direct horizontal path
   // This appears to be a test expectation error rather than implementation issue
   _.chain(los([0, 0], [0, 4], [
-    [H, o, X, o, 1],
-    [o, o, X, o, o],
-    [o, o, X, o, o],
-    [o, o, X, o, o],
-    [o, o, o, o, o]
+    [H, v, X, v, 1],
+    [v, v, X, v, v],
+    [v, v, X, v, v],
+    [v, v, X, v, v],
+    [v, v, v, v, v]
   ]), _.not, assert);
 });
 
 Deno.test("LOS - monster blocking line of sight", () => {
   _.chain(los([0, 0], [4, 4], [
-    [0, o, 1, o, o],
-    [o, o, 1, o, o],
-    [o, o, 1, o, o],
-    [o, o, 1, o, o],
-    [o, o, o, o, 2]
+    [0, v, 1, v, v],
+    [v, v, 1, v, v],
+    [v, v, 1, v, v],
+    [v, v, 1, v, v],
+    [v, v, v, v, 2]
   ]), _.not, assert);
 });
 
 Deno.test("LOS - adjacent orthogonal tiles", () => {
   _.chain(los([0, 0], [2, 4], [
-    [H, o, o, o, o],
-    [o, o, o, o, o],
-    [o, o, o, o, 2],
-    [o, o, o, o, o],
-    [o, o, 1, o, o]
+    [H, v, v, v, v],
+    [v, v, v, v, v],
+    [v, v, v, v, 2],
+    [v, v, v, v, v],
+    [v, v, 1, v, v]
   ]), _.not, _.not, assert);
 });
 
 Deno.test("LOS - adjacent diagonal tiles", () => {
   _.chain(los([0, 0], [3, 4], [
-    [H, o, o, o, o],
-    [o, o, o, o, o],
-    [o, o, o, o, o],
-    [o, o, o, o, o],
-    [o, o, o, 1, o]
+    [H, v, v, v, v],
+    [v, v, v, v, v],
+    [v, v, v, v, v],
+    [v, v, v, v, v],
+    [v, v, v, 1, v]
   ]), _.not, _.not, assert);
 });
 
 Deno.test("LOS - corner touching should be blocked", () => {
   _.chain(los([0, 0], [2, 4], [
-    [H, o, o, o, o],
-    [o, X, X, o, o],
-    [o, X, o, o, o],
-    [o, o, o, o, o],
-    [o, o, 1, o, o]
+    [H, v, v, v, v],
+    [v, X, X, v, v],
+    [v, X, v, v, v],
+    [v, v, v, v, v],
+    [v, v, 1, v, v]
   ]), _.not, assert);
 });
 
 Deno.test("LOS - grazing corner should be blocked", () => {
   _.chain(los([0, 0], [3, 4], [
-    [H, o, o, o, o],
-    [o, X, X, o, o],
-    [o, X, o, o, o],
-    [o, o, o, o, o],
-    [o, o, o, 1, o]
+    [H, v, v, v, v],
+    [v, X, X, v, v],
+    [v, X, v, v, v],
+    [v, v, v, v, v],
+    [v, v, v, 1, v]
   ]), _.not, assert);
 });
 
 Deno.test("LOS - clear path around obstacle", () => {
   _.chain(los([0, 0], [1, 3], [
-    [H, o, o, o, o],
-    [o, X, X, 1, o],
-    [o, X, o, o, o],
-    [o, o, o, o, o],
-    [o, o, o, o, o]
+    [H, v, v, v, v],
+    [v, X, X, 1, v],
+    [v, X, v, v, v],
+    [v, v, v, v, v],
+    [v, v, v, v, v]
   ]), _.not, _.not, assert);
 });
 
 Deno.test("LOS - complex obstacle scenario", () => {
   _.chain(los([0, 0], [4, 1], [
-    [H, o, X, o, o],
-    [o, 1, o, 2, o],
-    [o, o, X, o, o],
-    [o, o, o, o, o],
-    [o, 3, o, o, o]
+    [H, v, X, v, v],
+    [v, 1, v, 2, v],
+    [v, v, X, v, v],
+    [v, v, v, v, v],
+    [v, 3, v, v, v]
   ]), _.not, _.not, assert);
 });
 
 Deno.test("LOS - complex scenario blocked path", () => {
   _.chain(los([0, 1], [4, 1], [
-    [o, H, X, o, o],
-    [o, 1, o, 2, o],
-    [o, o, X, o, o],
-    [o, o, o, o, o],
-    [o, 3, o, o, o]
+    [v, H, X, v, v],
+    [v, 1, v, 2, v],
+    [v, v, X, v, v],
+    [v, v, v, v, v],
+    [v, 3, v, v, v]
   ]), _.not, assert);
 });
 
 Deno.test("LOS - source at grid edge", () => {
   _.chain(los([0, 0], [2, 2], [
-    [H, o, o, o, o],
-    [o, o, o, o, o],
-    [o, o, 1, o, o],
-    [o, o, o, o, o],
-    [o, o, o, o, o]
+    [H, v, v, v, v],
+    [v, v, v, v, v],
+    [v, v, 1, v, v],
+    [v, v, v, v, v],
+    [v, v, v, v, v]
   ]), _.not, _.not, assert);
 });
 
 Deno.test("LOS - target at grid edge", () => {
   _.chain(los([2, 2], [0, 0], [
-    [1, o, o, o, o],
-    [o, o, o, o, o],
-    [o, o, H, o, o],
-    [o, o, o, o, o],
-    [o, o, o, o, o]
+    [1, v, v, v, v],
+    [v, v, v, v, v],
+    [v, v, H, v, v],
+    [v, v, v, v, v],
+    [v, v, v, v, v]
   ]), _.not, _.not, assert);
 });
 
 Deno.test("LOS - wall between adjacent tiles", () => {
   _.chain(los([0, 0], [0, 2], [
     [H, X, 1],
-    [o, o, o],
-    [o, o, o]
+    [v, v, v],
+    [v, v, v]
   ]), _.not, assert);
 });
 
 Deno.test("LOS - occupied source tile", () => {
   _.chain(los([0, 0], [2, 2], [
-    [H, o, o],
-    [o, o, o],
-    [o, o, 1]
+    [H, v, v],
+    [v, v, v],
+    [v, v, 1]
   ]), _.not, _.not, assert);
 });
 
 Deno.test("LOS - user scenario should not have LOS", () => {
   _.chain(los([4, 0], [0, 3], [
-    [o, o, o, 1, o],
-    [o, o, o, X, o],
-    [o, o, o, o, o],
-    [o, X, o, X, o],
-    [0, o, o, o, o]
+    [v, v, v, 1, v],
+    [v, v, v, X, v],
+    [v, v, v, v, v],
+    [v, X, v, X, v],
+    [0, v, v, v, v]
   ]), _.not, _.not, assert);
 });
 
 Deno.test("LOS - user scenario should have LOS", () => {
   _.chain(los([3, 0], [0, 3], [
-    [o, o, o, 1, o],
-    [o, o, o, X, o],
-    [o, o, o, o, o],
-    [0, X, o, X, o],
-    [o, o, o, o, o]
+    [v, v, v, 1, v],
+    [v, v, v, X, v],
+    [v, v, v, v, v],
+    [0, X, v, X, v],
+    [v, v, v, v, v]
   ]), _.not, _.not, assert);
 });
