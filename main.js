79e5107b606115e33f09742366e3647b744bf9b8
Mario T. Lanza <me@mlanza.com>
2026-01-16 18:30:43 -0500

    save

--- main.js
+++ main.js
@@ -3,11 +3,9 @@ import $ from "./libs/atomic_/shell.js";
 import * as eff from "./libs/effects.js";
 import { reg, cmd } from "./libs/cmd.js";
 import * as o from "./ocd.js";
-const v = null;
-import {H, X, los} from "./ocd.js";
 
 const $state = $.atom(o.init());
-
+$.sub($state, _.comp(console.log, o.render));
 reg({ $state, o });
 
 $.swap($state, o.energize(eff.rolling(4, 5, 6)));
@@ -16,9 +14,4 @@ $.swap($state, o.assignEnergy("attack"));
 $.swap($state, o.assignEnergy("defense"));
 $.swap($state, o.teleport(1, [2, 0]));
 
-$.sub($state, _.comp(console.log, o.render));
-
-_.chain($state, _.deref, _.partial(o.locations, 1), console.log)
-
-//$.swap($state, o.move({type: "move", details: {occupant: o.H, offset: [-1, 0], speed: 2}}));
 cmd();
